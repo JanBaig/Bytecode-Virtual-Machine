b4feb557c90a07cbcb04731a36e9d7455e5794b3
JanatB <janatbaig2@gmail.com>
2022-11-21 18:01:28 -0500

    base set

--- CLOX/main.c
+++ CLOX/main.c
@@ -1,12 +1,16 @@
 #include "common.h" 
 #include "Chunk/chunk.h"
+#include "Debug/debug.h" 
 
 int main(int  argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
 	writeChunk(&chunk, OP_RETURN);
-	freeChunk(&chunk);
 	
+	disassembleChunk(&chunk, "Test Chunk");
+
+	freeChunk(&chunk);
+
 	return 0;
 }  
 
