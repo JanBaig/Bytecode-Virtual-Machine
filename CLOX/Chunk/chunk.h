f221036aadefcad486d624dceeb483254d7ad295
JanatB <janatbaig2@gmail.com>
2022-11-21 20:19:30 -0500

    compelted chapter#14

--- CLOX/Chunk/chunk.h
+++ CLOX/Chunk/chunk.h
@@ -2,24 +2,27 @@
 #define clox_chunk_h
 
 #include "../common.h"
+#include "../Value/value.h"
 
 typedef enum {
+	OP_CONSTANT,
 	OP_RETURN, 
 } OpCode;
 
 typedef struct { 
 	int count;
 	int capacity;
-	uint8_t* code;  // code contains address of a variable that is a dynamic array and contains byte sized ints
+	ValueArray constants;
+	uint8_t* code; 
+	int* lines;
 } Chunk; 
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk* chunk);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
 
-
-
-
+// code contains address of a variable that is a dynamic array and contains byte sized ints
 
--- CLOX/main.c
+++ CLOX/main.c
@@ -5,10 +5,12 @@
 int main(int  argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
+	int constant = addConstant(&chunk, 1.2);
+	writeChunk(&chunk, OP_CONSTANT, 123); // 1 byte op
+	writeChunk(&chunk, constant, 123); // 1 byte operand, 2 byte instruction format
+	writeChunk(&chunk, OP_RETURN, 123);
 	
 	disassembleChunk(&chunk, "Test Chunk");
-
 	freeChunk(&chunk);
 
 	return 0;
