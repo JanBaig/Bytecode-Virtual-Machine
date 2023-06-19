9723bbb32db5ff815da39c940457081817b7ebbd
JanatB <janatbaig2@gmail.com>
2023-06-18 22:55:53 -0400

    chp 19 progress

--- CLOX/compiler/compiler.c
+++ CLOX/compiler/compiler.c
@@ -200,6 +200,11 @@ static void number() {
 	emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+	// +1 and -2 trim the string quotation marks
+	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void unary() {
 	TokenType operatorType = parser.previous.type;
 
@@ -235,7 +240,7 @@ ParseRule rules[] = {
   [TOKEN_LESS]			= {NULL,     NULL,   PREC_COMPARISON},
   [TOKEN_LESS_EQUAL]	= {NULL,     binary, PREC_COMPARISON},
   [TOKEN_IDENTIFIER]	= {NULL,     NULL,   PREC_NONE},
-  [TOKEN_STRING]		= {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING]		= {string,     NULL,   PREC_NONE},
   [TOKEN_NUMBER]		= {number,   NULL,   PREC_NONE},
   [TOKEN_AND]			= {NULL,     NULL,   PREC_NONE},
   [TOKEN_CLASS]			= {NULL,     NULL,   PREC_NONE},
